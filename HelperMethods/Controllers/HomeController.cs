4e1d985006f2e6eb59b8f2b3a14e6362df5a6c45
zhongpeng <zhongpeng2000@gmail.com>
2016-09-06 23:29:57 -0700

    INITIAL

--- HelperMethods/Controllers/HomeController.cs
+++ HelperMethods/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HelperMethods.Models;
 
 namespace HelperMethods.Controllers
 {
@@ -10,10 +11,25 @@ namespace HelperMethods.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear"};
+            ViewBag.Cities = new string[] { "New York", "London", "Pairs" };
+            string message = "This is an HTML element: <input>";
 
-            return View();
+            return View((object)message);
         }
+
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View(person);
+        }
+
 
         public ActionResult About()
         {
---
+++ HelperMethods/Infrastracture/CustomHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastracture
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+            foreach ( string str in list)
+            {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper html, string msg)
+        {
+            string result = String.Format("This is the message: <p>{0}</p>", msg);
+            return new MvcHtmlString(result);
+        }
+    }
+}
